f7231550879c4f5d1a35d4da23e12ed1565deb8d
Chris Richmond <crichmond1989@gmail.com>
2018-09-27 23:05:22 -0500

    added docker-install.sh for ubuntu

---
+++ install-docker.sh
@@ -0,0 +1,7 @@
+apt update
+apt install curl
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt update
+apt install docker-ce
+usermod -aG docker ${USER}
